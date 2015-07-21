efddc79d92fdc8bd0cd7691a1ce28c1b47a8f900
Oab00 <oab93@hotmail.com>
2015-07-22 00:55:01 +0100

    first commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('pinterestApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
